99644886542c5d3cbdffb739c6c83e2ce34df98a
netl <juuso.metsavuori@gmail.com>
2020-04-20 22:33:11 +0300

    fixed initial values for hall-effect sensor, made calibration faster

--- app/halleffect.py
+++ app/halleffect.py
@@ -5,8 +5,8 @@ class halleffect():
     def __init__(self):
         self.h = ADC(0)
         self.timer = Timer(-1)
-        self.sensor_low = 150
-        self.sensor_high= 90
+        self.sensor_high = 150
+        self.sensor_low = 90
 
     def read(self):
         return self.h.read()
@@ -16,7 +16,7 @@ class halleffect():
         values = []
         for i in range(100):
             values.append(self.h.read())
-            utime.sleep(i/100) # try to avoid missing any cyclic patterns
+            utime.sleep(i/1000) # try to avoid missing any cyclic patterns
         self.sensor_low = min(values)*0.9
         self.sensor_high = max(values)*1.1
 
